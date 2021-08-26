4dfd6a0fee5526b7c467c18dc6c393f799b6c5a5
Marek Chałabis <marek.chalabis@tylko.com>
2021-08-26 21:08:01 +0200

    Setup models and urls

--- api/models.py
+++ api/models.py
@@ -2,19 +2,22 @@ from django.db import models
 from djmoney.models.fields import MoneyField
 
 
-class Dish(models.Model):
-    name = models.CharField(max_length=50)
+class ProductABC(models.Model):
     describe = models.CharField(max_length=250)
+    added_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Dish(ProductABC):
+    name = models.CharField(max_length=50)
     price = MoneyField(max_digits=6, decimal_places=2, default_currency='EUR')
     preparation_time = models.TimeField()
-    added_date = models.DateField()
-    modified_date = models.DateField
     vegetarian = models.BooleanField()
 
 
-class Menu(models.Model):
+class Menu(ProductABC):
     name = models.CharField(max_length=50, unique=True)
-    describe = models.CharField(max_length=250)
-    added_date = models.DateField()
-    modified_date = models.DateField
-    dishes = models.ManyToManyField(Dish)
+    dishes = models.ManyToManyField(Dish,related_name='menu', blank=True)
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
-from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+from api.views import DishModelViewSet, MenuModelViewSet
+
+router = DefaultRouter()
+router.register('dishes', DishModelViewSet, basename='dishes')
+router.register('menus', MenuModelViewSet, basename='menus')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v1/token-auth/', views.obtain_auth_token)
 ]
