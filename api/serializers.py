f249f99ed7e851bf11fafc99745acc6e394ec6e2
Marek Chałabis <marek.chalabis@tylko.com>
2021-08-29 16:19:21 +0200

    Add serializers

--- api/serializers.py
+++ api/serializers.py
@@ -14,11 +14,10 @@ class DishSerializer(serializers.ModelSerializer):
 
 
 class MenuSerializer(serializers.ModelSerializer):
-    dishes = DishSerializer(many=True, read_only=True) # todo test for this
 
     class Meta:
         ordering = ['-id']
         model = Menu
         fields = '__all__'
         read_only_fields = ("modified_date", "added_date")
-        extra_kwargs = {'dishes': {'required': False}}
+        depth = 1
