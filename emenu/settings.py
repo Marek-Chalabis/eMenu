c86acfc751a5b9f5c62d00683bd78b538f606aa3
Marek Chałabis <marek.chalabis@tylko.com>
2021-09-02 09:15:57 +0200

    Change page size and add throttle

--- emenu/settings.py
+++ emenu/settings.py
@@ -144,10 +144,18 @@ REST_FRAMEWORK = {
             'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 50,
+    'PAGE_SIZE': 25,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+        'user': '1000/day'
+    }
 }
 
 # CELERY
