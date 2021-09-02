0e4f777999cad889b5affb4a581bc08e571f1e9d
Marek Chałabis <marek.chalabis@tylko.com>
2021-09-02 15:24:14 +0200

    Add swagger documentation

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_swagger.views import get_swagger_view
 from api.views import DishModelViewSet, MenuModelViewSet
+from django.conf.urls import url
 
 router = DefaultRouter()
 router.register('dishes', DishModelViewSet, basename='dishes')
@@ -11,4 +12,5 @@ router.register('menus', MenuModelViewSet, basename='menus')
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/token-auth/', views.obtain_auth_token),
+    url('v1/documentation/', get_swagger_view(title='Pastebin API')),
 ]
